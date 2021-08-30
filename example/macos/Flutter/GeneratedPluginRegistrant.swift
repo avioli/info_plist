4f6358a879992502f2cfc24a700dc8ff42a629ef
Evo Stamatov <aviolito@gmail.com>
2021-08-30 12:50:08 +1000

    add macos as supported platform

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import info_plist
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InfoPlistPlugin.register(with: registry.registrar(forPlugin: "InfoPlistPlugin"))
+}
